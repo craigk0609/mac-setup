40eda2148f8dc493241e3629b539ef026fc01421
Craig Kennedy <craig.kennedy@samtec.com>
2021-02-10 21:50:50 -0500

    initial update

---
+++ lib/utilities.sh
@@ -0,0 +1,61 @@
+#! /usr/bin/env bash
+
+# Defines general utility functions.
+
+# Answers the full install path (including file name) for file name.
+# Parameters: $1 (required) - The file name.
+get_install_path() {
+  local file_name="$1"
+  local install_path=$(get_install_root "$file_name")
+  printf "$install_path/$file_name"
+}
+export -f get_install_path
+
+# Cleans work path for temporary processing of installs.
+clean_work_path() {
+  rm -rf "$MAC_OS_WORK_PATH"
+}
+export -f clean_work_path
+
+# Answers the root install path for file name.
+# Parameters: $1 (required) - The file name.
+get_install_root() {
+  local file_name="$1"
+  local file_extension=$(get_extension "$file_name")
+
+  # Dynamically build the install path based on file extension.
+  case $file_extension in
+    '')
+      printf "/usr/local/bin";;
+    'app')
+      printf "/Applications";;
+    'prefPane')
+      printf "/Library/PreferencePanes";;
+    'qlgenerator')
+      printf "/Library/QuickLook";;
+    *)
+      printf "/tmp/unknown";;
+  esac
+}
+export -f get_install_root
+
+# Answers the file or directory basename.
+# Parameters: $1 (required) - The file path.
+get_basename() {
+  printf "${1##*/}" # Answers file or directory name.
+}
+export -f get_basename
+
+# Answers the file extension.
+# Parameters: $1 (required) - The file name.
+get_extension() {
+  local name=$(get_basename "$1")
+  local extension="${1##*.}" # Excludes dot.
+
+  if [[ "$name" == "$extension" ]]; then
+    printf ''
+  else
+    printf "$extension"
+  fi
+}
+export -f get_extension
